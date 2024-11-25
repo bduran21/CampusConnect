bbff2c3dc4ec186d693babafe5cf2491a0176c5b
bduran21 <duranbraulio12@gmail.com>
2024-11-24 22:31:37 -0600

    push

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,27 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+import React, { useEffect } from "react";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import FriendsPage from "./pages/FriendsPage";
 import CalendarPage from "./pages/CalendarPage";
 import AboutUsPage from "./pages/AboutUsPage";
-import { ClerkProvider, RedirectToSignIn, SignedIn, SignedOut } from "@clerk/clerk-react";
+
+import { initializeUserData } from "./data/initializeData";
 
 function App() {
-  return (
-    <Routes>
-      {/* Regular Routes */}
-      <Route path="/" element={<HomePage />} />
-      <Route path="/friends" element={<FriendsPage />} />
-      <Route path="/calendar" element={<CalendarPage />} />
-      <Route path="/about-us" element={<AboutUsPage />} />
+  useEffect(() => {
+    initializeUserData(); // Ensure data is initialized in localStorage
+  }, []);
 
-      {/* Clerk Redirect Handling */}
-      <Route
-        path="/callback"
-        element={
-          <ClerkProvider>
-            <SignedIn>
-              <HomePage />
-            </SignedIn>
-            <SignedOut>
-              <RedirectToSignIn />
-            </SignedOut>
-          </ClerkProvider>
-        }
-      />
-    </Routes>
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/friends" element={<FriendsPage />} />
+        <Route path="/calendar" element={<CalendarPage />} />
+        <Route path="/about-us" element={<AboutUsPage />} />
+        <Route path="/*" element={<HomePage />} />
+      </Routes>
+    </Router>
   );
 }
 
--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,4 +1,3 @@
-// src/components/NavBar.js
 import React from "react";
 import { Link } from "react-router-dom";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
@@ -15,19 +14,13 @@ function NavBar() {
         <Link to="/friends" className="nav-link">Friends</Link>
         <Link to="/calendar" className="nav-link">Calendar</Link>
         <Link to="/about-us" className="nav-link">About Us</Link>
-        
-        {/* User button for signed-in users */}
         <SignedIn>
           <UserButton />
         </SignedIn>
-
-        {/* Sign-in button for signed-out users with a redirect URL */}
         <SignedOut>
-          <SignInButton 
-            mode="modal" 
+          <SignInButton
+            mode="modal"
             redirectUrl="https://bduran21.github.io/CampusConnect/#/"
-            afterSignInUrl="https://bduran21.github.io/CampusConnect/#/"
-            afterSignUpUrl="https://bduran21.github.io/CampusConnect/#/"
           />
         </SignedOut>
       </div>
