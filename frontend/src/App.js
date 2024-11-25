5feb1200cf1f73d37b4a3448f7ac547af0a18afe
bduran21 <duranbraulio12@gmail.com>
2024-11-24 20:51:02 -0600

    push

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,21 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import FriendsPage from "./pages/FriendsPage";
 import CalendarPage from "./pages/CalendarPage";
 import AboutUsPage from "./pages/AboutUsPage";
-
-import { initializeUserData } from "./data/initializeData";
+import { ClerkProvider, RedirectToSignIn, SignedIn, SignedOut } from "@clerk/clerk-react";
 
 function App() {
-  useEffect(() => {
-    initializeUserData(); // Ensure data is initialized in localStorage
-  }, []);
-
   return (
     <Routes>
+      {/* Regular Routes */}
       <Route path="/" element={<HomePage />} />
       <Route path="/friends" element={<FriendsPage />} />
       <Route path="/calendar" element={<CalendarPage />} />
       <Route path="/about-us" element={<AboutUsPage />} />
+
+      {/* Clerk Redirect Handling */}
       <Route
         path="/callback"
         element={
