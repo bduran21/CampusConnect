e12081eca62c1312a4e6f08ab4cc6a2ff88e4b9a
bduran21 <duranbraulio12@gmail.com>
2024-11-24 19:52:25 -0600

    updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,7 +18,7 @@ function App() {
       <Route path="/friends" element={<FriendsPage />} />
       <Route path="/calendar" element={<CalendarPage />} />
       <Route path="/about-us" element={<AboutUsPage />} />
-      <Route path="*" element={<Navigate to="/" />} />
+      <Route path="*" element={<HomePage to="/" />} />
     </Routes>
   );
 }
